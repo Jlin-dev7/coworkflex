f4574bb338b8e4040b63b1e49f1424e862a62387
Jlin-dev7 <jean7.koffi@uvci.edu.ci>
2026-07-02 02:16:14 +0200

    Update SpaceController.java

--- backend/backend/src/main/java/com/coworkflex/backend/controller/SpaceController.java
+++ backend/backend/src/main/java/com/coworkflex/backend/controller/SpaceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/spaces")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class SpaceController {
 
     private final SpaceService spaceService;
